be0bb4fa12d33834170bdcb2f379598fd853dd90
ChriskenM <chriskenmutuma0727@gmail.com>
2025-02-12 11:40:04 +0300

    feat(navbar): add navigation links for easier page toggling
    Improved navigation experience by adding links to the navbar, allowing users to switch between pages seamlessly. Enhances usability and accessibility.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :passwords, param: :token
+
+  # Session routes for sign in
+  get 'sign_in', to: 'sessions#new', as: 'sign_in'    # sign-in page
+  post 'sign_in', to: 'sessions#create'                 # sign-in action
+  delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'  # sign-out action
+
+  # Sign-up routes
+  get "sign_up", to: "users#new", as: :sign_up
+  post "users", to: "users#create"
+
+  resources :books, only: [:index, :show]
+  resources :borrowings, only: [:create, :destroy]
+
   get "home/user"
-  get "home/books"
   root "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
